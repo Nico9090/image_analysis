a0d5ee02007d7f5d3cd83d4afa8d29402d35fa74
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2024-05-15 23:26:52 -0400

    Update updatedpercent_bases.sh

--- updatedpercent_bases.sh
+++ updatedpercent_bases.sh
@@ -1,5 +1,5 @@
 bases={'A':0, 'T':0, 'C':0, 'G':0}
-with open('/root/nicko/seq_analysis#/prefetch/sra/SARS-COV-2/selectedline.txt','r')as f:
+with open('selectedline.txt','r')as f:
         for line in f:
                 for nucleotides in line:
                         if nucleotides=='A' or nucleotides=='T' or nucleotides=='C' or nucleotides=='G':
